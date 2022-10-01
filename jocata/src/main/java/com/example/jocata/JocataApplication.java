8fcec28c81a3d786a8f5118baf09d8b1bce1b244
unknown <gugulothuvenky@gmail.com>
2022-10-01 14:46:21 +0530

    2nd commit

---
+++ jocata/src/main/java/com/example/jocata/JocataApplication.java
@@ -0,0 +1,13 @@
+package com.example.jocata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JocataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JocataApplication.class, args);
+	}
+
+}
---
+++ jocata/src/test/java/com/example/jocata/JocataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jocata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JocataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
